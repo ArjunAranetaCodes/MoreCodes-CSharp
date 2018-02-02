1e8bb83535d9a7aa0570810ef4a2653910647080
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-02 12:57:07 +0800

    2018 Updates

---
+++ Conversions/problem1.cs
@@ -0,0 +1,19 @@
+//Problem 1: Write a program that converts a number to string.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            Console.WriteLine(num.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Conversions/problem2.cs
@@ -0,0 +1,19 @@
+//Problem 2: Write a program that converts a string to integer.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            Console.WriteLine(num.ToString());
+            Console.ReadKey();
+        }
+    }
+}
