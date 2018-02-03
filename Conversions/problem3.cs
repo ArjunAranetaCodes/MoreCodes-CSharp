f1d432c2dd6962c55f5bbe3c1aa34d7c969dcdd0
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-03 09:03:04 +0800

    2018 Updates

---
+++ Conversions/problem3.cs
@@ -0,0 +1,19 @@
+//Problem 3: Write a program that converts an integer to float.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            Console.WriteLine(Convert.ToDouble(num));
+            Console.ReadKey();
+        }
+    }
+}
