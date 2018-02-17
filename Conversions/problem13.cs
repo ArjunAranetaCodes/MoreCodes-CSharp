52f008385abdb9366d1c6a796bead91fd0125eb9
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-18 07:45:36 +0800

    2018 Updates

---
+++ Conversions/problem13.cs
@@ -0,0 +1,20 @@
+//Problem 13: Write a program that converts celsius to farenheit.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String word = "MoreCodes";
+            int strLength = word.Length;
+            Console.WriteLine("String Length: " + strLength.ToString());
+            Console.ReadKey();
+        }
+    }
+}
