29734d060e9c784b735803330a6fca1982fccf57
Arjun Araneta <arjunaraneta@gmail.com>
2018-10-11 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem8.cs
@@ -0,0 +1,40 @@
+﻿/*
+Problem 8: Write a program to print the number pattern of ones and zeros using loop.
+11111
+11111
+11011
+11111
+11111
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 5;
+            int col = 5;
+            for (int y = 0; y < row; y++)
+            {
+                for (int x = 0; x < col; x++)
+                {
+                    if (x == (row / 2) && y == (col / 2))
+                    {
+                        Console.Write("0");
+                    }
+                    else
+                    {
+                        Console.Write("1");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
