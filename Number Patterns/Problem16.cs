b3a5c9fcefa1a30a45f01814f1acf2938abf569a
Arjun Araneta <arjunaraneta@gmail.com>
2018-10-12 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem16.cs
@@ -0,0 +1,32 @@
+﻿/*
+Problem 16: Write a program to print the number pattern using loop.
+12345
+1234
+123
+12
+1
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 5;
+            for (int y = row; y > 0; y--)
+            {
+                for (int x = 0; x < y; x++)
+                {
+                    Console.Write(x + 1);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
