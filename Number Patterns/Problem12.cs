c03c712d093260d234528eeac89e4763f9c9ac3a
Arjun Araneta <arjunaraneta@gmail.com>
2018-10-12 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem12.cs
@@ -0,0 +1,32 @@
+﻿/*
+Problem 12: Write a program to print the pattern of asterisks using loop.
+*****
+****
+***
+**
+*
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 5;
+            for (int y = row; y > 0; y--)
+            {
+                for (int x = 0; x < y; x++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
