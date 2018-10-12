1d071a33101aa577ff5476239ee53b257b4da00c
Arjun Araneta <arjunaraneta@gmail.com>
2018-10-12 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem19.cs
@@ -0,0 +1,44 @@
+﻿/*
+Problem 19: Write a program to print the number pattern using loop.
+11 11
+11 11
+   
+11 11
+11 11
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 5;
+            int col = 5;
+            for (int y = 0; y < row; y++)
+            {
+                for (int x = 0; x < col; x++)
+                {
+                    if (col / 2 == x || row / 2 == y)
+                    {
+                        Console.Write(" ");
+                    }
+                    else if ((col % 2 == 0 && (col / 2) == x) || (row % 2 == 0 && (row / 2) == y))
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("1");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
