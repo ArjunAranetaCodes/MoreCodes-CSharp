84de4818f753e34e269b91734326cae21357795d
Arjun Araneta <arjunaraneta@gmail.com>
2018-10-12 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem18.cs
@@ -0,0 +1,54 @@
+﻿/*
+Problem 18: Write a program to print the number pattern using loop.
+  1  
+ 111
+11111
+ 111
+  1
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 0;
+            int x = 0;
+            int rows = 3;
+            int stars = 1;
+            int blank = rows - 1;
+
+            for (y = 1; y < rows * 2; y++)
+            {
+                for (x = 1; x <= blank; x++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (x = 1; x < stars * 2; x++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+
+                if (y < rows)
+                {
+                    blank--;
+                    stars++;
+                }
+                else
+                {
+                    blank++;
+                    stars--;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
